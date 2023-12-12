5cc692c865a719150518d2691b4686a1dfae0288
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-12-12 22:52:29 +0400

    fix github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://sakooooo.github.io',
+    base: '/sakooooo.github.io',
+});
