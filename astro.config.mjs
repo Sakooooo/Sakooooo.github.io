c0b281d588cedae03c1f8e24eb638b5dce6dcf6a
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-12-17 13:31:08 +0400

    i might give up at this point

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://sakooooo.github.io',
-  base: '/sakooooo.github.io'
-});
+export default defineConfig({});
