c7572601eaa217eb1b09814d46a3c953cc9db2fa
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-12-17 13:08:37 +0400

    LMao

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sakooooo.github.io',
-    base: '/sakooooo.github.io',
+  site: 'https://sakooooo.github.io',
+  base: '/sakooooo.github.io'
 });
