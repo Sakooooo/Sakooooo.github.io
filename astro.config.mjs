c2569c4946801fc03ba8b8772771adcc594b32cd
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-12-21 21:52:42 +0400

    add mdx for blogposts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-       site: 'https://sakooooo.github.io',
+  site: 'https://sakooooo.github.io',
+  integrations: [mdx()]
 });
