986d2f6b6b39a22469682ebd6d0576466a50f32a
Sako <78461130+Sakooooo@users.noreply.github.com>
2024-04-24 16:58:20 +0400

    nuke everything

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://sakooooo.github.io',
-  integrations: [mdx()]
-});
